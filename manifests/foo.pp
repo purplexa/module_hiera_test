91bc82865d555bac733361f729a431a5a4fe8ac8
Ryan Whitehurst <ryan@ryanwhitehurst.com>
2016-08-11 11:38:31 -0700

    Testing module data with hiera

---
+++ manifests/foo.pp
@@ -0,0 +1,3 @@
+class module_hiera_test::foo ($bar = 'fail') {
+  notice("module_hiera_test::foo::bar: ${bar}")
+}
---
+++ manifests/init.pp
@@ -0,0 +1,3 @@
+class module_hiera_test ($foobar = 'fail') {
+  notice("module_hiera_test::foobar: ${foobar}")
+}
